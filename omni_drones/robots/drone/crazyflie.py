f9eb8e66010c0922d3bd29fd562d1e9f01af4460
“lyttttt3333” <liyitong21@mails.tsinghua.edu.cn>
2023-09-26 23:31:57 +0800

    demo 1d control & fix crazyflie

--- omni_drones/robots/drone/crazyflie.py
+++ omni_drones/robots/drone/crazyflie.py
@@ -36,7 +36,8 @@ from omni_drones.robots.drone import MultirotorBase
 
 class Crazyflie(MultirotorBase):
 
-    # usd_path: str = get_assets_root_path() + "/Isaac/Robots/Crazyflie/cf2x.usd"
-    usd_path: str = ASSET_PATH + "/usd/cf2x_isaac.usd"
+    # NOTE: there are unexpedted behaviors when using the asset from Isaac Sim
+    usd_path: str = ASSET_PATH + "/usd/cf2x_pybullet.usd"
+    # usd_path: str = ASSET_PATH + "/usd/cf2x_isaac.usd"
     param_path: str = ASSET_PATH + "/usd/crazyflie.yaml"
     DEFAULT_CONTROLLER = DSLPIDController

316195d27b99d1f006c58a3cb176f62660d6f947
zhangruize <2290321870@qq.com>
2023-10-05 23:00:54 +0800

    update specs of 4 single-agent tasks

--- omni_drones/envs/inv_pendulum/inv_pendulum_hover.py
+++ omni_drones/envs/inv_pendulum/inv_pendulum_hover.py
@@ -63,11 +63,12 @@ class InvPendulumHover(IsaacEnv):
       energy consumption.
     - `spin`: Reward computed from the spin of the drone to discourage spinning.
     - `swing`: Reward computed from the swing of the payload to discourage swinging.
+    - `action_smoothness`: Reward that encourages smoother drone actions, computed based on the throttle difference of the drone.
 
     The total reward is
 
     ```{math}
-        r = r_\text{bar_up} + r_\text{pos} + r_\text{bar_up} * (r_\text{spin} + r_\text{swing}) + r_\text{effort}
+        r = r_\text{bar_up} + r_\text{pos} + r_\text{bar_up} * (r_\text{spin} + r_\text{swing}) + r_\text{effort} + r_\text{action_smoothness}
     ```
 
     ## Episode End
@@ -77,6 +78,15 @@ class InvPendulumHover(IsaacEnv):
 
     ## Config
     - `bar_length`: The length of the pendulum's bar (or pole).
+
+    ## Config
+
+    | Parameter               | Type  | Default       | Description |
+    |-------------------------|-------|---------------|-------------|
+    | `drone_model`           | str   | "hummingbird" | Specifies the model of the drone being used in the environment. |
+    | `bar_length`            | float | 1.0           | Length of the pendulum's bar. |
+    | `reward_distance_scale` | float | 1.2           | Scales the reward based on `target_payload_rpos`. |
+    | `time_encoding`         | bool  | True          | Indicates whether to include time encoding in the observation space. If set to True, a 4-dimensional vector encoding the current progress of the episode is included in the observation. If set to False, this feature is not included. |
 
     """
     def __init__(self, cfg, headless):

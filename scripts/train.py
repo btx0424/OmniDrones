76731f94e96e494fb8e609919ba6e5419335d311
btx0424 <btx0424@outlook.com>
2024-04-09 16:18:04 +0800

    fix hydra

--- scripts/train.py
+++ scripts/train.py
@@ -35,7 +35,7 @@ from torchrl.envs.transforms import TransformedEnv, InitTracker, Compose
 
 FILE_PATH = os.path.dirname(__file__)
 
-@hydra.main(config_path=FILE_PATH, config_name="train", version_base=None)
+@hydra.main(config_path=FILE_PATH, config_name="train")
 def main(cfg):
     OmegaConf.register_new_resolver("eval", eval)
     OmegaConf.resolve(cfg)

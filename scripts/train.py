f826486675158a52ed133c528bc3ac74d84370f8
Ecstasy-EC <yixicai@connect.hku.hk>
2025-03-26 10:54:23 +0100

    Remove debug prints

--- scripts/train.py
+++ scripts/train.py
@@ -44,10 +44,8 @@ def main(cfg):
         
     from omni_drones.envs.isaac_env import IsaacEnv
     env_class = IsaacEnv.REGISTRY[cfg.task.name]
-    print(1)
 
     base_env = env_class(cfg, headless=cfg.headless)
-    print(2)
 
     transforms = [InitTracker()]
 
@@ -59,7 +57,6 @@ def main(cfg):
     if cfg.task.get("ravel_obs_central", False):
         transform = ravel_composite(base_env.observation_spec, ("agents", "observation_central"))
         transforms.append(transform)
-    print(3)
 
     # optionally discretize the action space or use a controller
     action_transform: str = cfg.task.get("action_transform", None)
@@ -77,8 +74,6 @@ def main(cfg):
 
     env = TransformedEnv(base_env, Compose(*transforms)).train()
     env.set_seed(cfg.seed)
-
-    print(4)
 
     try:
         policy = ALGOS[cfg.algo.name.lower()](
@@ -110,7 +105,6 @@ def main(cfg):
         device=cfg.sim.device,
         return_same_td=True,
     )
-    print(5)
 
     @torch.no_grad()
     def evaluate(

5bd0510610dd1ab92bf6355de16fb99d49d77d98
Matthew Allen <AechPro@users.noreply.github.com>
2025-10-31 22:16:55 -0400

    Fix FisheyeCameraCfg Dataclass

--- omni_drones/sensors/config.py
+++ omni_drones/sensors/config.py
@@ -114,7 +114,7 @@ class FisheyeCameraCfg(PinholeCameraCfg):
 
     projection_type: str = "fisheye_polynomial"
     """Type of projection to use for the camera. Defaults to "fisheye_polynomial"."""
-    usd_params: UsdCameraCfg = UsdCameraCfg()
+    usd_params: UsdCameraCfg = field(default_factory=UsdCameraCfg)
     """Parameters for setting USD camera settings."""
 
 
